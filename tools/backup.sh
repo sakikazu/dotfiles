4fcacad00c7638fa75c4cb1059171f87853eecd7
Kazutaka Sakimura <sakikazu15@gmail.com>
2013-11-24 15:05:42 +0900

    modify backup.sh

--- tools/backup.sh
+++ tools/backup.sh
@@ -8,18 +8,42 @@
 
 localbackdir='/home/sakikazu/backup/'
 
-#
-# Serversman(182.163.58.103)
-#
+echo
+echo '#############################################'
+echo '# Serversman(182.163.58.103)'
+echo '#############################################'
 serversman='182.163.58.103'
 
-# Gitリポジトリ
-rsync -av -e 'ssh -p 10022' sakikazu@"$serversman":/home/sakikazu/bak_git_rep $localbackdir
+echo
+echo '# Gitリポジトリ'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/home/sakikazu/bak_git_rep $localbackdir
 
-# mysql dump
-rsync -av -e 'ssh -p 10022' sakikazu@"$serversman":/home/sakikazu/bak $localbackdir
+echo
+echo '# mysql dump'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/home/sakikazu/bak $localbackdir
+
+echo
+echo '# AdanHP (uploadディレクトリのみ)'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/usr/local/site/a-dan_v4/public/upload "$localbackdir"adanhp/
+
+echo
+echo '# Koisos (uploadディレクトリのみ)'
+target='koisos'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/usr/local/site/$target/public/upload "$localbackdir""$target"/
+
+echo
+echo '# HabitMachine (uploadディレクトリのみ)'
+target='habit-machine'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/usr/local/site/$target/public/upload "$localbackdir""$target"/
 
 
-# AdanHP
-# uploadファイルのみ
+echo
+echo '# Nichiko (uploadsディレクトリのみ)'
+target='nichiko'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/usr/local/site/$target/public/uploads "$localbackdir""$target"/
+
+echo
+echo '# Matabunkai (uploadsディレクトリのみ)'
+target='matabunkai'
+rsync -av --delete -e 'ssh -p 10022' sakikazu@"$serversman":/usr/local/site/$target/public/uploads "$localbackdir""$target"/
 

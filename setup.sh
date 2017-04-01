5816635a818b8507d366edbea81b592a80dc9cfd
sakikazu <sakikazu15@gmail.com>
2017-04-01 17:23:36 +0900

    oh-my-zshのディレクトリも管理に含めた

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/.vimrc ~
 ln -s ~/dotfiles/.zshrc ~
-ln -s ~/dotfiles/.screenrc ~
+#ln -s ~/dotfiles/.screenrc ~
 ln -s ~/dotfiles/.gemrc ~
 ln -s ~/dotfiles/.gitconfig ~
+ln -s ~/dotfiles/.oh-my-zsh ~

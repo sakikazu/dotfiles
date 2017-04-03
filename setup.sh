cfc2b21c1ebe8b8bd54786e1367897b25c711466
Kazutaka Sakimura <sakikazu15@gmail.com>
2017-04-03 11:00:27 +0900

    tiny fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+#
+# this script is able to run any number of times
+#
+
 ln -s ~/dotfiles/.vimrc ~
 ln -s ~/dotfiles/.zshrc ~
-#ln -s ~/dotfiles/.screenrc ~
 ln -s ~/dotfiles/.gemrc ~
 ln -s ~/dotfiles/.gitconfig ~
-ln -s ~/dotfiles/.oh-my-zsh ~
+
+echo
+echo "####################"
+echo "you need setup for oh-my-zsh cf. https://github.com/robbyrussell/oh-my-zsh if you didn't it"
+echo "####################"

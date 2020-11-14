d4e4ce531bb111a0d84c226854f71f4eba5cd7e2
k.sakimura <sakikazu15@gmail.com>
2020-11-14 22:39:55 +0900

    fix backup.rb

--- tools/backup.rb
+++ tools/backup.rb
@@ -15,26 +15,36 @@ class Backup
 
   DELETE_DAY = 5 # この数分の前の日のファイルを削除する
 
-  IS_DRY_RUN = false # trueならコマンドを実行しない
+  # is_dry_run: trueならコマンドを実行しない
+  def initialize(is_dry_run: false)
+    @is_dry_run = is_dry_run
+  end
 
-  def main
-    deleted_date = get_deleted_date
+  def execute
+    for_habit_machine
+    for_adan
+    for_gassprice
+  end
 
+  def for_habit_machine
     output_label(HM)
     exec_cmd("mkdir -p #{LOCAL_BAK_DIR}/#{HM}")
     backupfilepath = exec_cmd("ssh mysakura \"/usr/local/site/#{HM}/current/lib/backup4mysql.sh\"") { |result| result.chomp }
     exec_cmd("scp -p mysakura:#{backupfilepath} #{LOCAL_BAK_DIR}/#{HM}")
     exec_cmd("rsync -av --delete mysakura:/usr/local/site/#{HM}/shared/public/upload #{LOCAL_BAK_DIR}/#{HM}")
     delete_old_file("habitm_#{deleted_date}.sql.gz")
+  end
 
+  def for_adan
     output_label(ADAN)
     exec_cmd("mkdir -p #{LOCAL_BAK_DIR}/#{ADAN}")
     backupfilepath = exec_cmd("ssh mysakura \"/usr/local/site/#{ADAN}/current/lib/backup4mysql.sh\"") { |result| result.chomp }
     exec_cmd("scp -p mysakura:#{backupfilepath} #{LOCAL_BAK_DIR}/#{ADAN}")
     exec_cmd("rsync -av --delete mysakura:/usr/local/site/#{ADAN}/shared/public/upload #{LOCAL_BAK_DIR}/#{ADAN}")
     delete_old_file("adan_v4_#{deleted_date}.sql.gz")
+  end
 
-    # gassprice
+  def for_gassprice
     # ファイルのバックアップはなし
     output_label(GASSPRICE)
     exec_cmd("mkdir -p #{LOCAL_BAK_DIR}/#{GASSPRICE}")
@@ -47,14 +57,14 @@ class Backup
     puts "\n\n###### #{project}"
   end
 
-  def get_deleted_date
+  def deleted_date
     target_day = Date.today - DELETE_DAY.day
     short_year = target_day.year.to_s.slice(2, 2)
     md = target_day.strftime("%m%d")
     "#{short_year}#{md}"
   end
 
-  # TODO: やっぱりこれだと、スクリプトが実行されない時が続くと、削除されないファイルも溜まっていくので、以前のものはすべて、としたい
+  # TODO: やっぱりこれだと、スクリプトが実行されない日があると削除されないファイルも溜まっていくので、以前のものはすべて、としたい
   def delete_old_file(file)
     File.delete(file)
   rescue StandardError => e
@@ -62,18 +72,44 @@ class Backup
   end
 
   def exec_cmd(cmd)
-    stdout = if IS_DRY_RUN
+    stdout = if @is_dry_run
                puts "dry run: #{cmd}"
-               'test'
+               ''
              else
                `#{cmd}`
              end
+    puts "... #{stdout}" unless stdout.empty?
     yield stdout if block_given?
+  end
+
+  class << self
+    def show_help
+      puts "\n#### Usage ####\n"
+      puts ' -h: help'
+      puts ' --dry-run: コマンドを実行しない予行練習'
+      puts
+    end
   end
 end
 
-b = Backup.new
-b.main
+options = {}
+ARG_HELP = '-h'.freeze
+ARG_DRY_RUN = '--dry-run'.freeze
+unless ARGV.empty?
+  ARGV.each do |arg|
+    unless [ARG_HELP, ARG_DRY_RUN].include?(arg)
+      raise ArgumentError, "`#{arg}` is unable"
+    end
+  end
+  if ARGV.include?(ARG_HELP)
+    Backup.show_help
+    exit
+  elsif ARGV.include?(ARG_DRY_RUN)
+    options[:is_dry_run] = true
+  end
+end
+b = Backup.new(**options)
+b.execute
 
 
 # KNOWHOW
